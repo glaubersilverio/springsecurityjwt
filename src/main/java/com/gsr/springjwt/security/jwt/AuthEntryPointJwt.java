5768789d860f72b6b379cbed9509338d1345c4c0
Gláuber Silverio Ribeiro <glaubersilverio@gmail.com>
2020-11-19 18:13:04 -0300

    Personalizacao de retorno de acesso negado - AuthEntryPointJwt

--- src/main/java/com/gsr/springjwt/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/gsr/springjwt/security/jwt/AuthEntryPointJwt.java
@@ -1,11 +1,13 @@
 package com.gsr.springjwt.security.jwt;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
@@ -17,10 +19,16 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint{
 
 	private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
 		logger.error("Unauthorized error: {}", authException.getMessage());
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+		response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        JSONObject object = new JSONObject();
+        object.put("timestamp", LocalDateTime.now());
+        object.put("message", "Access Denied");
+		response.getWriter().write(object.toString());
 	}
 }
